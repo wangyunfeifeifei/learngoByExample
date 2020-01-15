e682bb3158066588ead3038aa7aee0fb67327cdb
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2020-01-15 13:44:03 +0800

    完成一大半了

---
+++ goroutines/goroutines.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+	f("dirct")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println("done")
+}
